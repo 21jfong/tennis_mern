695fd851890c89804703f87f14408b7106f521d7
21jfong <21jfong@gmail.com>
2024-10-23 22:33:58 -0600

    try to fix links for mobile

--- client/src/components/Teams/Teams.js
+++ client/src/components/Teams/Teams.js
@@ -40,18 +40,24 @@ const Teams = ({ setIsAlert, setAlertMessage }) => {
 
   return (
     <Grow in>
-      <Grid2 container spacing={3}>
-        <Grid2 container>
+      <Grid2 container spacing={3} sx={{ flexDirection: 'column', padding: '16px' }}>
+        <Grid2 item xs={12} sx={{ marginBottom: '20px' }}>
           <Button variant="contained" color="primary" onClick={handleCreateTeam}>Create Team</Button>
+        </Grid2>
+        <Grid2 item xs={12} sx={{ marginBottom: '20px' }}>
           <Button variant="contained" color="primary" onClick={handleJoinTeam}>Join Team</Button>
         </Grid2>
-        <Container className={classes.mainContainer}>
+        <Container className={classes.mainContainer} sx={{ padding: '16px' }}>
           <Typography variant='h4'>My Teams</Typography>
           {teams.length > 0 ? teams?.map((team) => (
-            <Link href={`my-teams/${team._id}`} key={team._id}>
+            <Link href={`my-teams/${team._id}`} key={team._id} sx={{ display: 'block', marginBottom: '8px' }}>
               {`${team.name} - Captain: ${team.captain.name}`} <br />
             </Link>
-          )) : <Typography sx={{ fontSize: 'clamp(1.5rem, 2.5vw, 2.5rem)' }}>No Teams Available</Typography>}
+          )) : (
+            <Typography sx={{ fontSize: 'clamp(1.5rem, 2.5vw, 2.5rem)' }}>
+              No Teams Available
+            </Typography>
+          )}
         </Container>
       </Grid2>
     </Grow>
