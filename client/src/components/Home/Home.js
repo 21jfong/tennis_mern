bf4f09bedc6152ef0ed060912db85933ba487e15
21jfong <21jfong@gmail.com>
2024-11-13 22:32:04 -0700

    misc

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -7,13 +7,14 @@ import {
   Box,
   IconButton,
   Fade,
+  Button,
 } from "@mui/material";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
-import florin1 from "../../images/florin1.webp";
+import florin4 from "../../images/florin1.webp";
 import florin2 from "../../images/florin2.webp";
 import florin3 from "../../images/florin3.webp";
-import florin4 from "../../images/florin4.webp";
+import florin1 from "../../images/florin4.webp";
 import florin5 from "../../images/florin5.webp";
 import florin6 from "../../images/florin6.webp";
 
@@ -21,6 +22,7 @@ const Home = () => {
   const photos = [florin1, florin2, florin3, florin4, florin5, florin6];
 
   const [currentPhotoIndex, setCurrentPhotoIndex] = useState(0);
+  const [view, setView] = useState(false);
   const totalPhotos = photos.length;
 
   const handleNext = () => {
@@ -45,51 +47,72 @@ const Home = () => {
         </CardContent>
       </Card>
 
+      {view ? (
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          position="relative"
+          width="100%"
+          maxWidth="300px"
+          mx="auto"
+        >
+          <IconButton
+            onClick={handlePrev}
+            sx={{
+              position: "absolute",
+              left: 0,
+              top: "50%",
+              transform: "translateY(-50%)",
+            }}
+          >
+            <ArrowBackIosIcon />
+          </IconButton>
+          {photos.map((photo, index) => (
+            <Fade in={index === currentPhotoIndex} key={index} timeout={500}>
+              <Box
+                component="img"
+                src={photo}
+                alt={`Photo ${index + 1}`}
+                sx={{
+                  display: index === currentPhotoIndex ? "block" : "none",
+                  width: "100%",
+                  borderRadius: 2,
+                }}
+              />
+            </Fade>
+          ))}
+          <IconButton
+            onClick={handleNext}
+            sx={{
+              position: "absolute",
+              right: 0,
+              top: "50%",
+              transform: "translateY(-50%)",
+            }}
+          >
+            <ArrowForwardIosIcon />
+          </IconButton>
+        </Box>
+      ) : null}
+
       <Box
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-        position="relative"
-        width="100%"
-        maxWidth="300px"
-        mx="auto"
+        sx={{
+          position: "fixed",
+          bottom: 20,
+          width: "fit-content",
+        }}
       >
-        <IconButton
-          onClick={handlePrev}
+        <Button
+          variant="contained"
+          onClick={() => setView(!view)}
           sx={{
-            position: "absolute",
-            left: 0,
-            top: "50%",
-            transform: "translateY(-50%)",
+            width: "150px",
+            borderRadius: "8px",
           }}
         >
-          <ArrowBackIosIcon />
-        </IconButton>
-        {photos.map((photo, index) => (
-          <Fade in={index === currentPhotoIndex} key={index} timeout={500}>
-            <Box
-              component="img"
-              src={photo}
-              alt={`Photo ${index + 1}`}
-              sx={{
-                display: index === currentPhotoIndex ? "block" : "none",
-                width: "100%",
-                borderRadius: 2,
-              }}
-            />
-          </Fade>
-        ))}
-        <IconButton
-          onClick={handleNext}
-          sx={{
-            position: "absolute",
-            right: 0,
-            top: "50%",
-            transform: "translateY(-50%)",
-          }}
-        >
-          <ArrowForwardIosIcon />
-        </IconButton>
+          {view ? "Hide Florin" : "View Florin"}
+        </Button>
       </Box>
     </Grid2>
   );
