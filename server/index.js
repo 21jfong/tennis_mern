d9bc122dcdee38875232b8f8e8898d05536faf11
Jordan Fong <21jfong@gmail.com>
2025-04-26 15:36:05 -0600

    open connection

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import dotenv from 'dotenv'
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
 
-import userRoutes from "./routes/users.js"
-import teamsRoutes from "./routes/teams.js"
-import matchesRoutes from "./routes/matches.js"
+import userRoutes from "./routes/users.js";
+import teamsRoutes from "./routes/teams.js";
+import matchesRoutes from "./routes/matches.js";
 
 dotenv.config();
 const app = express();
@@ -15,23 +15,28 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 const corsOptions = {
-  origin: process.env.ORIGIN,  // Allow requests only from your frontend
+  origin: process.env.ORIGIN, // Allow requests only from your frontend
   methods: "GET,POST,PATCH,DELETE",
   credentials: true,
 };
 app.use(cors(corsOptions));
 
 // goes to localhost:5000/posts
-app.use('/user', userRoutes);
-app.use('/my-teams', teamsRoutes);
-app.use('/', matchesRoutes);
+app.use("/user", userRoutes);
+app.use("/my-teams", teamsRoutes);
+app.use("/", matchesRoutes);
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send("hello to tennis api");
-})
+});
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.CONNECTION_URL, {})
-  .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
+mongoose
+  .connect(process.env.CONNECTION_URL, {})
+  .then(() =>
+    app.listen(PORT, "0.0.0.0", () =>
+      console.log(`Server running on port ${PORT}`)
+    )
+  )
   .catch((error) => console.log(error.message));
